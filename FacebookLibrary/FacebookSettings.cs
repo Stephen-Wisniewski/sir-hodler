3180a7f449f1123d472ce045f9b1164e0f226f76
Stephen-Wisniewski <stephen.p.wisniewski@gmail.com>
2018-02-27 15:13:45 +0000

    Added functionality to post to the Sir Hodler Facebook wall.

---
+++ FacebookLibrary/FacebookSettings.cs
@@ -0,0 +1,7 @@
+namespace ConsoleApplication
+{
+    public static class FacebookSettings
+    {
+        public static string AccessToken = "EAACEdEose0cBAOkMqq9HFNbwXYqejH2P6JRAjMZCHZCS7wZASoujKpWaZBZBLMTe4TEaR5mhEpZAafLcTpalBLI8rah4rvIBKUJ5P301T1WwNQaZBJwZBVmoD5L70k0eZABze01mtwBeQeBgHwAkTrpYThaJPLtyY04LG3ZBQP3wfYmO5i3aknSRJ6RumhD0y1M8apOZBlXvDPdSAZDZD";
+    }
+}
--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ConsoleApplication;
 using Newtonsoft.Json.Linq;
 using SirHodlerBot;
 
@@ -12,7 +13,15 @@ namespace SirHodlerBot
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(Message.GetQuoteOfTheDay());
+            var facebookClient = new FacebookClient();
+            var facebookService = new FacebookService(facebookClient);
+            var getAccountTask = facebookService.GetAccountAsync(FacebookSettings.AccessToken);
+            Task.WaitAll(getAccountTask);
+            var account = getAccountTask.Result;
+            Console.WriteLine(account.Id);
+
+            var postOnWallTask = facebookService.PostOnWallAsync(FacebookSettings.AccessToken, "I'm alive!");
+
 
             Console.ReadLine();
         }
