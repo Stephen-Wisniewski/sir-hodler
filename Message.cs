b8934a83a6ba80f180845f0edbe763ee5cc28206
Stephen-Wisniewski <stephen.p.wisniewski@gmail.com>
2018-03-05 23:56:31 +0000

    Minor fixes

--- Message.cs
+++ Message.cs
@@ -64,7 +64,7 @@ namespace SirHodlerBot
             string Coin = (string)JObj["name"];
             string Change7Days = (string)JObj["percent_change_7d"];
 
-            string UpOrDown = ((Func<string>)(() => {if(Double.Parse(Change7Days) < 0) return "down"; return "up"; }))();
+            string UpOrDown = ((Func<string>)(() => { if (Double.Parse(Change7Days) < 0) return "down"; return "up"; }))();
 
             return $"This weeks coin of of the week is {Coin}.\n\nIt's gone {UpOrDown} by {Change7Days} percent in the last 7 days!";
         }
